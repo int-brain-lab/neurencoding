cf400b1926965b9bf33fbec2b44c9993ec54bd7e
Berk Gercek <gercek.berk@gmail.com>
2022-11-30 17:44:27 +0100

    Added nonlinear basis functions

--- neurencoding/utils.py
+++ neurencoding/utils.py
@@ -11,6 +11,13 @@ from brainbox.io.one import SessionLoader
 
 
 def raised_cosine(duration, nbases, binfun):
+    """
+    Create raised cosine basis functions of durection `duration` with `nbases` bases.
+    
+    The `binfun` argument is a function that takes a duration in seconds and returns the number of
+    bins in that given duration. Often implemented as e.g. `np.ceil(t / dt).astype(int)`. Needs
+    to return an integer.
+    """
     nbins = binfun(duration)
     ttb = repmat(np.arange(1, nbins + 1).reshape(-1, 1), 1, nbases)
     dbcenter = nbins / nbases
@@ -22,6 +29,17 @@ def raised_cosine(duration, nbases, binfun):
 
 
 def full_rcos(duration, nbases, binfun, n_before=1):
+    """
+    Create full raised cosine basis functions of durection `duration` with `nbases` bases.
+
+    This differs from `raised_cosine` in that the basis functions are not restricted to have a
+    center within the duration of the window, and `n_before` bases are added before the beginning
+    of the basis window.
+    
+    The `binfun` argument is a function that takes a duration in seconds and returns the number of
+    bins in that given duration. Often implemented as e.g. `np.ceil(t / dt).astype(int)`. Needs
+    to return an integer.
+    """
     if not isinstance(n_before, int):
         n_before = int(n_before)
     nbins = binfun(duration)
@@ -31,6 +49,25 @@ def full_rcos(duration, nbases, binfun, n_before=1):
     bcenters = 0.5 * dbcenter + dbcenter * np.arange(-n_before, nbases - n_before)
     x = ttb - repmat(bcenters.reshape(1, -1), nbins, 1)
     bases = (np.abs(x / cwidth) < 0.5) * (np.cos(x * np.pi * 2 / cwidth) * 0.5 + 0.5)
+    return bases
+
+
+
+def nonlinear_rcos(duration, nbases, nloffset, binfun):
+    if nloffset <= 0:
+        raise ValueError('nloffset must be positive and nonzero')
+    def basisfun(x, c, dc):
+        inner = np.fmax(-np.pi, np.fmin(np.pi, np.pi * (x - c) / (2 * dc)))
+        return (np.cos(inner) + 1) / 2
+    nbins = binfun(duration)
+    nlin = lambda x: np.log(x + 1e-20)
+    nlininv = lambda x: np.exp(x) - 1e-20
+    yrange = nlin(np.array([0, binfun(duration)]) + binfun(nloffset))
+    dbcenter = (yrange[1] - yrange[0]) / (nbases - 1)
+    centers = np.arange(yrange[0], yrange[1] + dbcenter, dbcenter)
+    maxt = nlininv(yrange[1] + 2 * dbcenter) - binfun(nloffset)
+    samplet = nlin(np.arange(nbins) + binfun(nloffset))
+    bases = basisfun(samplet.reshape(-1, 1), centers.reshape(1, -1), dbcenter)
     return bases
 
 
@@ -102,6 +139,7 @@ def bincount2D(x, y, xbin=0, ybin=0, xlim=None, ylim=None, weights=None):
 
     return r, xscale, yscale
 
+
 def load_trials_df(
     eid,
     one=None,
@@ -111,8 +149,8 @@ def load_trials_df(
     ret_abswheel=False,
     wheel_binsize=0.02,
     addtl_types=[],
-    trials_mask=None, 
-    ):
+    trials_mask=None,
+):
     """
     Generate a pandas dataframe of per-trial timing information about a given session.
     Each row in the frame will correspond to a single trial, with timing values indicating timing
@@ -183,8 +221,8 @@ def load_trials_df(
     trialsdf = trials[trialstypes]
     if trials_mask is not None:
         trialsdf = trialsdf.loc[trials_mask]
-    trialsdf["trial_start"] = trialsdf['stimOn_times'] - t_before
-    trialsdf["trial_end"] = trialsdf['feedback_times'] + t_after
+    trialsdf["trial_start"] = trialsdf["stimOn_times"] - t_before
+    trialsdf["trial_end"] = trialsdf["feedback_times"] + t_after
     tdiffs = trialsdf["trial_end"] - np.roll(trialsdf["trial_start"], -1)
     if np.any(tdiffs[:-1] > 0):
         logging.warning(
@@ -205,7 +243,7 @@ def load_trials_df(
         wh_endind = np.searchsorted(whlt[wh_endlast:], end, side="right") + wh_endlast + 4
         wh_endlast = wh_endind
         tr_whlvel = whlv[wh_startind:wh_endind]
-        tr_whlt = whlt[wh_startind : wh_endind] - start
+        tr_whlt = whlt[wh_startind:wh_endind] - start
         whlseries = TimeSeries(tr_whlt, tr_whlvel, columns=["whlvel"])
         whlsync = sync(wheel_binsize, timeseries=whlseries, interp="previous")
         trialstartind = np.searchsorted(whlsync.times, 0)
@@ -220,9 +258,9 @@ def load_trials_df(
     trialsdf["wheel_velocity"] = trials
     return trialsdf
 
-class SequentialSelector:
 
-    def __init__(self, model, n_features_to_select=None, direction='forward', scoring=None):
+class SequentialSelector:
+    def __init__(self, model, n_features_to_select=None, direction="forward", scoring=None):
         """
         Sequential feature selection for neural models
 
@@ -250,7 +288,7 @@ class SequentialSelector:
         self.direction = direction
         self.scoring = scoring
         self.trlabels = self.design.trlabels
-        if hasattr(self.model, 'traininds'):
+        if hasattr(self.model, "traininds"):
             self.train = np.isin(self.trlabels, self.model.traininds).flatten()
             self.test = ~self.train
         else:
@@ -275,35 +313,50 @@ class SequentialSelector:
             Whether to show a progress bar, by default False
         """
         if train_idx is None and self.train is None:
-            raise ValueError('train_idx cannot be None if model used to create SFS did not have '
-                             'any training indices')
+            raise ValueError(
+                "train_idx cannot be None if model used to create SFS did not have "
+                "any training indices"
+            )
         if train_idx is not None:
             self.train = np.isin(self.trlabels, train_idx).flatten()
             self.test = ~self.train
         n_features = len(self.features)
         maskdf = pd.DataFrame(index=self.model.clu_ids, columns=self.features, dtype=bool)
         maskdf.loc[:, :] = False
-        seqdf = pd.DataFrame(index=self.model.clu_ids, columns=range(self.n_features_to_select))
-        trainscoredf = pd.DataFrame(index=self.model.clu_ids,
-                                    columns=range(self.n_features_to_select))
-        testscoredf = pd.DataFrame(index=self.model.clu_ids,
-                                   columns=range(self.n_features_to_select))
+        ncols = (
+            self.n_features_to_select
+            if self.direction == "forward"
+            else n_features - self.n_features_to_select
+        )
+        seqdf = pd.DataFrame(index=self.model.clu_ids, columns=range(ncols))
+        trainscoredf = pd.DataFrame(index=self.model.clu_ids, columns=range(ncols))
+        testscoredf = pd.DataFrame(index=self.model.clu_ids, columns=range(ncols))
 
         if not 0 < self.n_features_to_select <= n_features:
-            raise ValueError('n_features_to_select is not a valid number in the context'
-                             ' of the model.')
+            raise ValueError(
+                "n_features_to_select is not a valid number in the context" " of the model."
+            )
 
-        n_iterations = (self.n_features_to_select if self.direction == 'forward' else n_features -
-                        self.n_features_to_select)
+        n_iterations = (
+            self.n_features_to_select
+            if self.direction == "forward"
+            else n_features - self.n_features_to_select
+        )
+        if self.direction == "backward":
+            self.model.fit(train_idx=train_idx, printcond=False)
+            self.basescores = self.model.score()
         if full_scores:
-            fullindex = pd.MultiIndex.from_product([self.model.clu_ids, np.arange(n_iterations)],
-                                                   names=['clu_id', 'feature_iter'])
-            fulltrain = pd.DataFrame(index=fullindex, columns=range(self.n_features_to_select))
-            fulltest = pd.DataFrame(index=fullindex, columns=range(self.n_features_to_select))
+            fullindex = pd.MultiIndex.from_product(
+                [self.model.clu_ids, np.arange(n_iterations)], names=["clu_id", "feature_iter"]
+            )
+            fulltrain = pd.DataFrame(index=fullindex, columns=range(n_features))
+            fulltest = pd.DataFrame(index=fullindex, columns=range(n_features))
 
-        for i in tqdm(range(n_iterations), desc='step', leave=False, disable=not progress): # loop in rpogress 
-            masks_set = maskdf.groupby(self.features.tolist()).groups 
-            for current_mask in tqdm(masks_set, desc='feature subset', leave=False):
+        for i in tqdm(
+            range(n_iterations), desc="step", leave=False, disable=not progress
+        ):  # loop in rpogress
+            masks_set = maskdf.groupby(self.features.tolist()).groups
+            for current_mask in tqdm(masks_set, desc="feature subset", leave=False):
                 cells = masks_set[current_mask]
                 outputs = self._get_best_new_feature(current_mask, cells, full_scores)
                 if full_scores:
@@ -316,8 +369,8 @@ class SequentialSelector:
                     trainscoredf.loc[cell, i] = nf_train.loc[cell]
                     testscoredf.loc[cell, i] = nf_test.loc[cell]
                     if full_scores:
-                        fulltest.loc[cell, i] = nf_fulltest
-                        fulltrain.loc[cell, i] = nf_fulltrain
+                        fulltest.loc[cell, i] = nf_fulltest.loc[cell]
+                        fulltrain.loc[cell, i] = nf_fulltrain.loc[cell]
         self.support_ = maskdf
         self.sequences_ = seqdf
         self.scores_test_ = testscoredf
@@ -325,8 +378,9 @@ class SequentialSelector:
         if full_scores:
             self.full_scores_train_ = fulltrain
             self.full_scores_test_ = fulltest
-        self.deltas_train_ = self._compute_deltas(self.scores_train_, self.sequences_)
-        self.deltas_test_ = self._compute_deltas(self.scores_test_, self.sequences_)
+        if not self.direction == "backward":
+            self.deltas_train_ = self._compute_deltas(self.scores_train_, self.sequences_)
+            self.deltas_test_ = self._compute_deltas(self.scores_test_, self.sequences_)
         # TODO: Add line here that actually computes Rsq of mean model based on train set when scored on train set,
         # then subtract that from the first column of self.deltas_test
 
@@ -346,38 +400,41 @@ class SequentialSelector:
         for feature_idx in candidate_features:
             candidate_mask = mask.copy()
             candidate_mask[feature_idx] = True
-            if self.direction == 'backward':
+            if self.direction == "backward":
                 candidate_mask = ~candidate_mask
             fitfeatures = self.features[candidate_mask]
-            feat_idx = np.hstack([self.design.covar[feat]['dmcol_idx'] for feat in fitfeatures])
+            feat_idx = np.hstack([self.design.covar[feat]["dmcol_idx"] for feat in fitfeatures])
             mdm = self.design[np.ix_(self.train, feat_idx)]
-            mdm_test = self.design[np.ix_(self.test, feat_idx)] # select the rows self.test and columns feat_idx 
+            mdm_test = self.design[
+                np.ix_(self.test, feat_idx)
+            ]  # select the rows self.test and columns feat_idx
 
             coefs, intercepts = self.model._fit(mdm, my, cells=cells)
             for i, cell in enumerate(cells):
-                trainscores.at[cell,
-                               feature_idx] = self.model._scorer(coefs.loc[cell],
-                                                                 intercepts.loc[cell], mdm, my[:,
-                                                                                               i])
-                testscores.at[cell,
-                              feature_idx] = self.model._scorer(coefs.loc[cell],
-                                                                intercepts.loc[cell], mdm_test,
-                                                                my_test[:, i])
+                trainscores.at[cell, feature_idx] = self.model._scorer(
+                    coefs.loc[cell], intercepts.loc[cell], mdm, my[:, i]
+                )
+                testscores.at[cell, feature_idx] = self.model._scorer(
+                    coefs.loc[cell], intercepts.loc[cell], mdm_test, my_test[:, i]
+                )
 
         maxind = trainscores.idxmax(axis=1)
         trainmax = trainscores.max(axis=1)
         # Ugly kludge to compensate for DataFrame.lookup being deprecated
         midx, cols = pd.factorize(maxind)
-        testmax = pd.Series(testscores.reindex(cols, axis=1).to_numpy()[np.arange(len(testscores)),
-                                                                        midx],
-                            index=testscores.index)
+        testmax = pd.Series(
+            testscores.reindex(cols, axis=1).to_numpy()[np.arange(len(testscores)), midx],
+            index=testscores.index,
+        )
         if full_scores:
             return maxind, trainmax, testmax, trainscores, testscores
         else:
             return maxind, trainmax, testmax
 
     @staticmethod
-    def _compute_deltas(scores, sequences):
+    def _compute_deltas(scores, sequences, direction='forward'):
+        if direction == "backwards":
+            raise NotImplementedError("No support for backwards delta computation for now")
         n_cov = scores.shape[1]
         diffs = pd.DataFrame(index=scores.index, columns=range(n_cov))
         for i in range(n_cov):
@@ -385,11 +442,13 @@ class SequentialSelector:
                 diffs[i] = scores[i]
             else:
                 diffs[i] = scores[i] - scores[i - 1]
-        diffmelt = pd.melt(diffs, ignore_index=False, var_name='position',
-                           value_name='diff').set_index('position', append=True)
-        posmelt = pd.melt(sequences, ignore_index=False, var_name='position',
-                          value_name='covname').set_index('position', append=True)
-        joindf = diffmelt.join(posmelt, how='inner')
+        diffmelt = pd.melt(
+            diffs, ignore_index=False, var_name="position", value_name="diff"
+        ).set_index("position", append=True)
+        posmelt = pd.melt(
+            sequences, ignore_index=False, var_name="position", value_name="covname"
+        ).set_index("position", append=True)
+        joindf = diffmelt.join(posmelt, how="inner")
         assert len(joindf) == len(diffmelt)
         deltadf = joindf.droplevel("position").pivot(columns="covname", values="diff")
         return deltadf
